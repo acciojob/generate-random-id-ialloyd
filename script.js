335bfbd6b0dd06441b422b1581f9a886855d0f03
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-12-07 19:29:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function makeid(l) {
   // write your code here
+	let i=1
+	let arr='abcdefghijklmnopqrstuvwxyz0123456789'.split('')
+	let res=[]
+	
+	while(i<=l){
+		res.push(arr[Math.floor(Math.random()*36)])
+		i++;
+	}
+
+	return res.join('')
+	
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+ alert(makeid(l));
